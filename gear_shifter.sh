e42a144ef77e42abc9561b81bcbd93010eb15687
MohammedAzharKK <azramn6993@gmail.com>
2026-04-06 16:49:38 +0530

    feat: implement dynamic CPU-based coloring for gear shifter and overhaul Waybar styling with glassmorphism and floating pills

--- gear_shifter.sh
+++ gear_shifter.sh
@@ -5,10 +5,25 @@ active_ws=$(hyprctl activeworkspace -j)
 id=$(echo "$active_ws" | jq -r '.id')
 name=$(echo "$active_ws" | jq -r '.name')
 
-# Define the shifter layout
+# Get CPU usage for "revving" effect
+cpu_usage=$(grep 'cpu ' /proc/stat | awk '{print ($2+$4)*100/($2+$4+$5)}' | cut -d. -f1)
+
 gears=("R" "N" "1" "2" "3" "4" "5" "6")
 
-output="["
+# Dynamic Colors based on "RPM" (CPU Usage)
+if [[ $cpu_usage -gt 70 ]]; then
+    ACTIVE_COLOR="#f38ba8" # High RPM Red
+elif [[ $cpu_usage -gt 30 ]]; then
+    ACTIVE_COLOR="#fab387" # Mid RPM Orange
+else
+    ACTIVE_COLOR="#f9e2af" # Low RPM Yellow
+fi
+
+R_COLOR="#f38ba8"
+N_COLOR="#94e2d5"
+D_COLOR="#89b4fa"
+
+output="󱊟 <span color='#585b70'>[</span>"
 for i in "${!gears[@]}"; do
     gear="${gears[$i]}"
     is_active=false
@@ -16,26 +31,34 @@ for i in "${!gears[@]}"; do
     case "$gear" in
         "R")
             if [[ "$name" == special:* ]]; then is_active=true; fi
+            color=$R_COLOR
             ;;
         "N")
-            # Active if id is not 1-6 and not special
-            if [[ ! "$id" =~ ^[1-6]$ && ! "$name" == special:* ]]; then is_active=true; fi
+            # Neutral if no windows are open in current workspace
+            windows=$(echo "$active_ws" | jq -r '.windows')
+            if [[ "$windows" -eq 0 && ! "$name" == special:* ]]; then is_active=true; fi
+            color=$N_COLOR
             ;;
         *)
-            if [[ "$id" == "$gear" ]]; then is_active=true; fi
+            if [[ "$id" == "$gear" && "$name" != special:* ]]; then 
+                # Only active if windows are present
+                windows=$(echo "$active_ws" | jq -r '.windows')
+                if [[ "$windows" -gt 0 ]]; then is_active=true; fi
+            fi
+            color=$D_COLOR
             ;;
     esac
 
     if $is_active; then
-        output+="<span color='#f38ba8'><b>$gear</b></span>"
+        output+="<span color='$ACTIVE_COLOR'><b>$gear</b></span>"
     else
-        output+="$gear"
+        output+="<span color='$color'>$gear</span>"
     fi
     
     if [[ $i -lt $((${#gears[@]} - 1)) ]]; then
-        output+=" "
+        output+="<span color='#45475a'>·</span>"
     fi
 done
-output+="]"
+output+="<span color='#585b70'>]</span>"
 
 echo "$output"
